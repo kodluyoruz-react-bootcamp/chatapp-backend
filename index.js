4fe2f5dc22baf07105834d48eebc4576b8d323ce
Mehmet Seven <mehmetseven0@gmail.com>
2021-01-17 19:11:03 +0300

    readme file

---
+++ index.js
@@ -0,0 +1,31 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+const Messages = require("./lib/Messages");
+
+app.get("/", (req, res) => {
+	res.end("Merhaba Socket.IO");
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+
+	Messages.list((data) => {
+		console.log(data);
+		socket.emit("message-list", data);
+	});
+
+	socket.on("new-message", (message) => {
+		console.log(message);
+		Messages.upsert({ message });
+
+		socket.broadcast.emit("receive-message", message);
+	});
+
+	socket.on("disconnect", () => console.log("a user disconnected"));
+});
+
+http.listen(3000, () => {
+	console.log("listening on *:3000");
+});
