f8fd9a63c35d4c47178c5d6d5d343c8122ccecd1
Mehmet Seven <mehmetseven0@gmail.com>
2021-04-25 12:32:32 +0300

    node port config

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", () => console.log("a user disconnected"));
 });
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || "3000", () => {
 	console.log("listening on *:3000");
 });
