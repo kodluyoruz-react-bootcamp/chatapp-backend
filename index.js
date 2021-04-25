fc715f9cd433392486bbc088eaeb5685610a3a20
Mehmet Seven <mehmetseven0@gmail.com>
2021-04-25 12:52:03 +0300

    cors

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const cors = require("cors");
 
 const Messages = require("./lib/Messages");
 
-app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.end("Merhaba Socket.IO");
